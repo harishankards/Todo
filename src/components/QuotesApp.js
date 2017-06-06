0d5ce8acf51a76327a8dc64036c2fbe6fdc8c0f6
harishankards <harishankards@gmail.com>
2017-06-06 17:01:17 +0530

    Add little good css and quotes app

---
+++ src/components/QuotesApp.js
@@ -0,0 +1,16 @@
+import React, {Component} from 'react';
+import '../App.css';
+class QuotesApp extends Component{
+  constructor(props){
+    super(props);
+  }
+
+  render(){
+    return(
+      <div className="quotes-app"></div>
+    );
+  }
+}
+
+
+export default QuotesApp;
